4074b80db756a5b12f523fa5f835acf565710337
Gabriel <gabriel.slmendoza@gmail.com>
2022-08-21 22:52:29 -0500

    hello world

---
+++ src/components/hello-world/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+import { MdFavoriteBorder } from 'react-icons/md'
+
+export function HelloWorld(): JSX.Element {
+	return (
+		<Container>
+			<MdFavoriteBorder /> Hello World!
+		</Container>
+	)
+}
---
+++ src/components/hello-world/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1rem;
+    & svg {
+        margin-right: 5px;
+    }
+`
